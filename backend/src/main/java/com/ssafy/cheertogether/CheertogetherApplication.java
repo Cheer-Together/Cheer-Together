9cd7a444046f4a0b126305bedcdffc4975d18d33
choijoohee <choijoohee@naver.com>
2022-07-25 14:06:42 +0900

    chore: 프로젝트 생성 및 초기 설정

---
+++ backend/src/main/java/com/ssafy/cheertogether/CheertogetherApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.cheertogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheertogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheertogetherApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/cheertogether/CheertogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.cheertogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheertogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
