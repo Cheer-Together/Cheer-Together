46c72a243533647342cd026d7ad8d8196e7bb4db
홍성덕 <tjdejr338@naver.com>
2022-07-26 15:01:15 +0900

    chore<FE>: 메인페이지 작업 중

---
+++ frontend/src/components/FavoriteTeam.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="FavoriteTeam">
+    <div class="FavoriteTeam-title">
+      좋아하는 팀
+    </div>
+    <div class="FavoriteTeam-section">
+      <div class="FavoriteTeam-section-title">
+        로그인 후 이용해 주세요!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.FavoriteTeam {
+  margin-bottom: 21px;
+}
+.FavoriteTeam-title {
+  font-size: 18px;
+  padding: 5.5px 0;
+}
+.FavoriteTeam-section {
+  margin-top: 11px;
+  width: 1580px;
+  height: 170px;
+  text-align: center;
+  background-color: var(--sub-color);
+}
+.FavoriteTeam-section-title {
+  padding-top: 78px ;
+}
+</style>
