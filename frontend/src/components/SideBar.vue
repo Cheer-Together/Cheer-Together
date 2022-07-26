52c41421011c3bd3ae14bf7abc813c52137bfde8
홍성덕 <tjdejr338@naver.com>
2022-07-26 11:31:24 +0900

    chore<FE>: 메인페이지 스켈레톤 생성

---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="SideBar">
+    사이드 바
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style >
+.SideBar {
+  width: 210px;
+  min-height: 889px;
+  max-height: 3000px;
+  background-color: blueviolet;
+}
+</style>
