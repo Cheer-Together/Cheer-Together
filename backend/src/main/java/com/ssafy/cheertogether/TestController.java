217a45232975726a5290e3fac818ed90bf25a813
Il kwon <alexryu1105@naver.com>
2022-07-27 10:38:47 +0900

    feat<BE>: 브랜치 확인을 위한 테스트 파일 추가

---
+++ backend/src/main/java/com/ssafy/cheertogether/TestController.java
@@ -0,0 +1,13 @@
+package com.ssafy.cheertogether;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+	@GetMapping("/")
+	String hello(){
+		return "index";
+	}
+}
