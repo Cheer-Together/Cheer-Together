a6b752d18afe61ef6fc3e1aaa5bca98a3f5addff
choijoohee213 <choijoohee@naver.com>
2022-07-31 04:03:12 +0900

    feat<BE>: 회원 로그인 api 구현 (JWT 토큰 로그인)

---
+++ backend/src/main/java/com/ssafy/cheertogether/common/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package com.ssafy.cheertogether.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.GenericFilter;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.ssafy.cheertogether.auth.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilter {
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws
+		IOException,
+		ServletException {
+		String token = jwtTokenProvider.resolveToken((HttpServletRequest)request);
+
+		if (token != null && jwtTokenProvider.validateToken(token)) {
+			Authentication auth = jwtTokenProvider.getAuthentication(token);
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		}
+		System.out.println(request);
+		System.out.println(response);
+		chain.doFilter(request, response);
+	}
+}
---
+++ backend/src/main/java/com/ssafy/cheertogether/member/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.ssafy.cheertogether.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequest {
+	private String email;
+	private String password;
+}
