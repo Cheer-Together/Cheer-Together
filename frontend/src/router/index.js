8f10d86819985815429eb913e9a9a842fab693b0
홍성덕 <tjdejr338@naver.com>
2022-07-31 10:43:38 +0900

    feat<FE>: 회원가입 페이지 좋아하는 리그까지 디자인

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainPageView from '../views/MainPageView.vue'
 import ArticleView from '../views/ArticleView.vue'
 import ScheduleView from '../views/ScheduleView.vue'
+import SignupView from '../views/SignupView.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/schedule',
     name: 'Schedule',
     component: ScheduleView
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: SignupView
   },
 ]
 
