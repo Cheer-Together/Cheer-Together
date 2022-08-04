0f86b22c87116263779b6040702a6933292aab64
choijoohee213 <choijoohee@naver.com>
2022-08-04 15:07:25 +0900

    refactor<BE>: 회원 로그인 시에 회원 정보와 jwt 토큰 정보를 반환하도록 변경

---
+++ backend/src/main/java/com/ssafy/cheertogether/member/dto/MemberLoginResponse.java
@@ -0,0 +1,34 @@
+package com.ssafy.cheertogether.member.dto;
+
+import java.util.List;
+
+import com.ssafy.cheertogether.favorite.domain.FavoriteLeague;
+import com.ssafy.cheertogether.member.domain.Member;
+
+import lombok.Getter;
+
+@Getter
+public class MemberLoginResponse {
+	private final Long id;
+	private final String email;
+	private final String nickname;
+	private final String profileImage;
+	private final String role;
+	private final String myInfo;
+	private final List<FavoriteLeague> favoriteLeagueList;
+	private String jwtToken;
+
+	public MemberLoginResponse(Member member) {
+		id = member.getId();
+		email = member.getEmail();
+		nickname = member.getNickname();
+		profileImage = member.getNickname();
+		role = member.getRole();
+		myInfo = member.getMyInfo();
+		favoriteLeagueList = member.getFavoriteLeagueList();
+	}
+
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+}
