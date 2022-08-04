b63138c70f7bbd6921c044695e99b4176c5aa9f4
choijoohee213 <choijoohee@naver.com>
2022-08-04 22:13:44 +0900

    refactor<BE>: Match가 mysql 예약어여서 Game으로 변경

--- backend/src/main/java/com/ssafy/cheertogether/game/dto/GameModifyRequest.java
+++ backend/src/main/java/com/ssafy/cheertogether/game/dto/GameModifyRequest.java
@@ -1,16 +1,16 @@
-package com.ssafy.cheertogether.match.dto;
+package com.ssafy.cheertogether.game.dto;
 
 import java.time.LocalDateTime;
 
-import com.ssafy.cheertogether.match.domain.MatchStatus;
+import com.ssafy.cheertogether.game.domain.GameStatus;
 
 import lombok.Getter;
 
 @Getter
-public class MatchModifyRequest {
+public class GameModifyRequest {
 	private LocalDateTime kickoff;
 	private String stadium;
-	private MatchStatus status;
+	private GameStatus status;
 	private Integer homeScore;
 	private Integer awayScore;
 	private Long apiId;
