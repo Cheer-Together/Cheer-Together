fb205a7bf12af6a970db7473237fc2d3a24b12b6
CJS <joungsu9876@naver.com>
2022-08-09 13:10:29 +0900

    fix<BE> : 게시글  ApiId 자료형 수정

--- backend/src/main/java/com/ssafy/cheertogether/article/repository/ArticleRepository.java
+++ backend/src/main/java/com/ssafy/cheertogether/article/repository/ArticleRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ssafy.cheertogether.article.domain.Article;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-	List<Article> findArticlesByLeague_ApiId(int leagueApiId, Sort sort);
+	List<Article> findArticlesByLeague_ApiId(Long leagueApiId, Sort sort);
 
 	List<Article> findArticlesByTitleContainingOrContentContaining(String title, String content, Sort sort);
 }
