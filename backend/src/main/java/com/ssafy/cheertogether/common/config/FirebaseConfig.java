1ffa57267256d1611db1b59d9ed3c46b1e45caab
choijoohee213 <choijoohee@naver.com>
2022-08-09 23:34:48 +0900

    fix<BE>: 파이어베이스 설정값을 부르는 변수 오타 수정

--- backend/src/main/java/com/ssafy/cheertogether/common/config/FirebaseConfig.java
+++ backend/src/main/java/com/ssafy/cheertogether/common/config/FirebaseConfig.java
@@ -18,7 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FirebaseConfig {
 
-	@Value("${app.firebase-configuration-file")
+	@Value("${app.firebase-configuration-file}")
 	private String firebaseConfigPath;
 
 	@PostConstruct
