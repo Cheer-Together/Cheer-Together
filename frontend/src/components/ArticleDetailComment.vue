4347288e1cad16e2b5b6d39d74d9117834283009
tetepiti149 <tetepiti149@naver.com>
2022-08-11 14:37:51 +0900

    feat<FE>: 게시글 삭제 추가

--- frontend/src/components/ArticleDetailComment.vue
+++ frontend/src/components/ArticleDetailComment.vue
@@ -1,22 +1,22 @@
 <template>
-  <div style="display: flex; height: 40px; margin-bottom: 5px;">
+  <div v-if="loaded" style="display: flex; height: 40px; margin-bottom: 5px;">
     <!-- <div style="height: 40px; width:40px; margin-right: 6px; background-color:aquamarine;">
     </div> -->
     <div style="font-size:13px; margin-left: 10px;">
       <div style="color:#8D8D8D; display: flex;">
         {{replyNickName}} | {{replyCreatedDate}}
-        <div style="margin-left:10px" class="word-link">
-            <v-icon>
-              mdi-pencil
-            </v-icon>
-            <a>수정하기</a>
-          </div>
-          <div class="word-link" style="margin-left:5px">
-            <v-icon>
-              mdi-delete
-            </v-icon>
-            <a>삭제하기</a>
-          </div>
+        <div v-if="loginUserId==props.reply.memberId" style="margin-left:10px" class="word-link">
+          <v-icon>
+            mdi-pencil
+          </v-icon>
+          <a>수정하기</a>
+        </div>
+        <div v-if="loginUserId==props.reply.memberId" @click="deleteComment()" class="word-link" style="margin-left:5px">
+          <v-icon>
+            mdi-delete
+          </v-icon>
+          <a>삭제하기</a>
+        </div>
       </div>
       <div>
         {{props.reply.content}}
@@ -28,11 +28,16 @@
 <script setup>
 import axios from 'axios'
 import { defineProps, ref } from 'vue'
+import jwt_decode from "jwt-decode"
+import router from '@/router';
 const props = defineProps({
   reply: Object
 })
 const replyNickName = ref('')
 const replyCreatedDate = ref('')
+const loginUserId = jwt_decode(sessionStorage.getItem('token')).sub
+const loaded = ref(false)
+
 const createdTime = new Date(props.reply.createDate)
 let yy = createdTime.getFullYear()
 let mm = createdTime.getMonth()
@@ -46,11 +51,26 @@ axios({
   url: 'https://i7b204.p.ssafy.io/cheertogether/members/' + props.reply.memberId,
   method: 'GET', 
 }).then(res => {
-  console.log(res)
   replyNickName.value = res.data.nickname
+  loaded.value = true
 }).catch(err => {
   console.log(err)
 })
+
+function deleteComment() {
+  axios({
+    url: 'https://i7b204.p.ssafy.io/cheertogether/articles/reply/' + props.reply.id,
+    method: 'DELETE',
+    params: {
+      jwtToken: sessionStorage.getItem('token')
+    }
+  }).then(res => {
+    console.log(res)
+    router.go()
+  }).catch(err => {
+    console.log(err)
+  })
+}
 </script>
 
 <style>
