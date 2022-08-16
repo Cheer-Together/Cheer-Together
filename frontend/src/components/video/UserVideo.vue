ea2fb20decd066dd38a0287b9a4dcc59382fb317
홍성덕 <tjdejr338@naver.com>
2022-08-16 22:12:14 +0900

    refactor<FE>: 방장 강퇴기능 수정

--- frontend/src/components/video/UserVideo.vue
+++ frontend/src/components/video/UserVideo.vue
@@ -1,15 +1,16 @@
 <template>
   <div v-if="streamManager" class="cam-range">
     <ov-video :stream-manager="streamManager" class="cam" />
-    <div class="cam-user-name" @click="onEmit()">
+    <div class="cam-user-name" >
       <!-- 이름 클릭하면 방장일 경우 강퇴 조치 -->
-      {{ clientData }}
+      {{ clientData }} <span id="emitIcon"  @click="onEmit(clientData)">⛔</span>
     </div>
   </div>
 </template>
 
 <script>
 import OvVideo from "./OvVideo";
+import Swal from "sweetalert2";
 
 export default {
   name: "UserVideo",
@@ -34,8 +35,27 @@ export default {
       const { connection } = this.streamManager.stream;
       return JSON.parse(connection.data);
     },
-    onEmit() {
-      this.$emit("forceOut", this.clientData);
+    onEmit(clientData) {
+      Swal.fire({
+        title: '정말 퇴장시키시겠습니까?',
+        text: '닉네임 : ' + clientData,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '네. 내보낼래요!',
+        cancelButtonText: '아뇨. 괜찮아요.'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.$emit("forceOut", this.clientData);
+          Swal.fire(
+            clientData + '님을 \n 내보냈습니다!',
+            '',
+            'success'
+          )
+        }
+      })
+      
     },
   },
 };
@@ -50,18 +70,18 @@ export default {
   border-radius: 5px;
   margin: 0 10px;
 }
-
-.cam-range:hover > .cam-user-name {
-  display: block;
+#emitIcon:hover {
+  cursor: pointer;
 }
-
 .cam-user-name {
+  margin: 0 10px;
+  width: 240px;
   font-size: 20px;
   font-family: var(--bold-font);
   /* position: absolute;
   top: 0;
   right: 10px; */
-  display: none;
   text-align: center;
+  background-color: #FEFBF6;
 }
 </style>
