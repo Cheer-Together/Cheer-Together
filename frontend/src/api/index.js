4ca8c4ca80ac108d88ccfd13fa85f7a9f2cb445e
choijoohee213 <choijoohee@naver.com>
2023-06-21 02:17:51 +0900

    [FE] feat: api 요청 시 인터셉트로 액세스 토큰 만료면 재발급 요청 #350

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { API_HOST_URL } from "@/config";
+import { useAccountStore } from "@/store/index.js";
+import jwt_decode from "jwt-decode";
 
 function apiInstance() {
   const instance = axios.create({
@@ -8,6 +10,41 @@ function apiInstance() {
       "Content-type": "application/json",
     },
   });
+
+  return setInterceptors(instance);
+}
+
+function setInterceptors(instance) {
+  instance.interceptors.request.use(
+    async function (config) {
+      var decoded = jwt_decode(useAccountStore().accessToken);
+      let now = new Date();
+      let expiry =
+        decoded.exp - Number(now.getTime().toString().substring(0, 10));
+      
+      if(expiry <= 0) {
+        await useAccountStore().reissueToken();
+      }
+
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  instance.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function (error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      return Promise.reject(error);
+    }
+  );
   return instance;
 }
 
