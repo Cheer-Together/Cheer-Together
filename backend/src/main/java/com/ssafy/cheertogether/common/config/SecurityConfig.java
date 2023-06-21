5bea540bf368cde9de02cd0fd379260f203dac67
choijoohee213 <choijoohee@naver.com>
2023-06-22 01:31:51 +0900

    #352 [BE] feat: 스프링 시큐리티 api별 권한 설정 추가

--- backend/src/main/java/com/ssafy/cheertogether/common/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/cheertogether/common/config/SecurityConfig.java
@@ -1,42 +1,46 @@
 package com.ssafy.cheertogether.common.config;
 
+import com.ssafy.cheertogether.common.filter.JwtAuthenticationFilter;
+import com.ssafy.cheertogether.member.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.ssafy.cheertogether.member.JwtTokenProvider;
-import com.ssafy.cheertogether.common.filter.JwtAuthenticationFilter;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.cors.CorsUtils;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private final JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.httpBasic().disable()
-			.authorizeRequests()// 요청에 대한 사용권한 체크
-			.antMatchers("/**").permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-				UsernamePasswordAuthenticationFilter.class); // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
-		// + 토큰에 저장된 유저정보를 활용하여야 하기 때문에 CustomUserDetailService 클래스를 생성합니다.
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()// 요청에 대한 사용권한 체크
+                .antMatchers("/members/*", "/oauth2/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/rooms/*").permitAll()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class); // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
+        // + 토큰에 저장된 유저정보를 활용하여야 하기 때문에 CustomUserDetailService 클래스를 생성합니다.
 
-	}
+    }
 }
